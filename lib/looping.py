2bfcb50894c8f080b3084bfc18bd2eada351b7e4
abyola <abyola46@gmail.com>
2024-04-25 13:46:08 +0300

    Completed Lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
+
+
+def fizzbuzz_printer():
+    for num in range(1, 101):
+        print(fizzbuzz(num))
+
+def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+
+def reverse_string(string):
+    reversed_str = ""
+    for char in string:
+        reversed_str = char + reversed_str
+    return reversed_str
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
 
-def fizzbuzz():
-    # code goes here!
-    pass
+fizzbuzz_printer()
